5efd828c7cb2eda3bd755561fcfc77d93836ebf7
Abdulrahman Gaoba <abdulrahman1997abdo@gmail.com>
2023-12-21 00:54:08 +0500

    added clerk auth

--- lms-platform/app/page.tsx
+++ lms-platform/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { Dangrek } from 'next/font/google'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className='bg-sky-100 w-full min-h-screen'>
       <p className='text-3xl'>hello</p>
-      <Button variant= "secondary" >Click here</Button>
+      <Button variant="secondary" >Click here</Button>
+      <UserButton afterSignOutUrl="/" />
+
     </div>
   )
 }
